fd6d691c5cc680748416a64383779e4f0121b03f
MSI\babbi <babbigiovanni@gmail.com>
2026-03-02 11:21:41 +0100

    Refactored magic number in GoldBankAccount

--- src/main/java/it/unibo/pps/e1/GoldBankAccount.java
+++ src/main/java/it/unibo/pps/e1/GoldBankAccount.java
@@ -2,6 +2,7 @@ package it.unibo.pps.e1;
 
 public class GoldBankAccount {
 
+    public static final int OVERDRAFT_LIMIT = 500;
     private final BankAccount base;
 
     public GoldBankAccount(BankAccount base) {
@@ -10,7 +11,7 @@ public class GoldBankAccount {
     public void deposit(int amount) { base.deposit(amount); }
     public int getBalance() { return base.getBalance(); }
     public void withdraw(int amount) {
-        if (this.getBalance() + 500 < amount) {
+        if (this.getBalance() + OVERDRAFT_LIMIT < amount) {
             throw new IllegalStateException();
         }
         base.withdraw(amount);
